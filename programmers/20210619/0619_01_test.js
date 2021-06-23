3d0982dc7724e0d743844b295475876e0d45f8ed
jeongbeen2 <aceman9508@gmail.com>
2021-06-24 01:01:58 +0900

    folder setting

---
+++ programmers/20210619/0619_01_test.js
@@ -0,0 +1,12 @@
+function solution(answers) {
+  let len = answers.length;
+  let first = [1, 2, 3, 4, 5];
+  let second = [2, 1, 2, 3, 2, 4, 2, 5];
+  let third = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
+  let first_ans = 0;
+  let second_ans = 0;
+  let third_ans = 0;
+  for (let i = 0; i < len; i++) {}
+  var answer;
+  return answer;
+}
---
+++ programmers/test.js
@@ -0,0 +1,6 @@
+let num = 10;
+if (num % 2 === 0) {
+  console.log('짝수 ㅋ');
+} else {
+  console.log('홀수');
+}
