cb65673adf251fe17d80600d1088c190d0fad8af
jeongbeen2 <aceman9508@gmail.com>
2021-07-04 17:37:17 +0900

    20210704

--- programmers/2021_07/20210704/0704_02_bigNum.js
+++ programmers/2021_07/20210704/0704_02_bigNum.js
@@ -1,14 +1,15 @@
+const changeFunc = (num) => {
+  let temp = '';
+  num = num.toString();
+  for (let i = 0; i < 4; i++) {
+    temp += num;
+  }
+  return temp;
+};
+
 function solution(numbers) {
   // 4자리 숫자를 만들어 준 다음, 정렬 해주고 다시 원상태의 넘버로 바꿔줘야함.
   let result = [];
-  const changeFunc = (num) => {
-    let temp = '';
-    num = num.toString();
-    for (let i = 0; i < 4; i++) {
-      temp += num;
-    }
-    return temp;
-  };
   for (number of numbers) {
     result.push(changeFunc(number));
   }
@@ -22,6 +23,7 @@ function solution(numbers) {
   if (!parseInt(result.join(''))) return '0';
   return result.join('');
 }
+
 let output = solution([0, 0, 0, 0, 0]);
 console.log(output); // --> '6210'
 
