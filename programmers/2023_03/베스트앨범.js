8c7302ab4323df321fa119635318f662caa12566
jeongbeen2 <aceman9508@gmail.com>
2023-03-22 19:39:43 +0900

    20230322

---
+++ programmers/2023_03/베스트앨범.js
@@ -0,0 +1,8 @@
+function solution(genres, plays) {
+    var answer = [];
+    return answer;
+}
+
+
+let result = solution(["classic", "pop", "classic", "classic", "pop"], [500, 600, 150, 800, 2500]);
+console.log(result); // [4, 1, 3, 0]
